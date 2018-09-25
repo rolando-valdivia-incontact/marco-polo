b3566f1a629cc9078d40d628538dedcb3a96e887
bnelford <beau.nelford@incontact.com>
2018-09-24 20:58:58 -0600

    first checkin, npm express mocha chai jenkins file

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/marco', function (req, res) {
+  res.send('Polo!');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
