85d9bcc33602fbb5c27eb31a1d9d92e759f86b70
bnelford <beau.nelford@incontact.com>
2018-09-25 11:32:47 -0600

    Changing port on deployed container

--- integrationtest/integrationtest.js
+++ integrationtest/integrationtest.js
@@ -1,19 +1,19 @@
 var request = require('supertest');
 
-describe('Running integration tests on the locally deployed app', function() {
+describe('Running integration tests on the deployed app', function() {
     it('responds to /marco with Polo!', function testSlash(done) {
-        request('http://localhost:46141')
+        request('http://localhost:3000')
           .get('/marco')
           .expect('Polo!')
           .expect(200, done);
     });
     it('404 everything else', function testPath(done) {
-        request('http://localhost:46141')
+        request('http://localhost:3000')
           .get('/foo/bar')
           .expect(404, done);
     });
     it('404 /', function testPath(done) {
-        request('http://localhost:46141')
+        request('http://localhost:3000')
           .get('/')
           .expect(404, done);
     });
